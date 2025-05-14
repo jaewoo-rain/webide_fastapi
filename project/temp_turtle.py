f2395523606c0e5e98dc9a26860ae8ffbb5f4b9c
jaewoo-rain <202017499@jbnu.ac.kr>
2025-05-14 17:24:16 +0900

    aaa

--- project/temp_turtle.py
+++ project/temp_turtle.py
@@ -1,29 +1,22 @@
 import turtle
 
 screen = turtle.Screen()
-screen.bgcolor("white")
-screen.setup(width=900, height=600)
+screen.bgcolor("black")
 
-t = turtle.Turtle()
-t.shape("turtle")
-t.color("blue")
-t.speed(10)
+star = turtle.Turtle()
+star.color("yellow")
+star.speed(3)
 
-left_edge = -screen.window_width() // 2
-right_edge = screen.window_width() // 2
+def draw_star(size):
+    for i in range(5):
+        star.forward(size)
+        star.right(144)
 
-def move_turtle():
-    t.forward(10)
-    
-    if t.xcor() > right_edge:
-        t.setx(right_edge)
-        t.right(180)
-    elif t.xcor() < left_edge:
-        t.setx(left_edge)
-        t.right(180)
-    
-    screen.ontimer(move_turtle, 20)
+for i in range(5):
+    draw_star(100)
+    star.penup()
+    star.forward(150)
+    star.right(144)
+    star.pendown()
 
-move_turtle()
-
-screen.mainloop()
+turtle.done()
